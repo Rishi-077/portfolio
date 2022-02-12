f14b520e1ddc84b0a8ed54b1294986864e7b1e84
Rishi-077 <rishikesan070@gmail.com>
2022-02-13 03:04:54 +0530

    home content completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Home from './components/Home';
 import Navitem from './components/Navitem';
 
 function App() {
   return (
     <div className="App">
       <Navitem />
+      <Home />
     </div>
   );
 }
