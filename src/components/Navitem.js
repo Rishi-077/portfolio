d446daf9fe64643e5c85644d9c813383d4d93260
Rishi-077 <rishikesan070@gmail.com>
2022-02-13 19:00:50 +0530

    some changes in about page

--- src/components/Navitem.js
+++ src/components/Navitem.js
@@ -8,8 +8,8 @@ function Navitem() {
       <Navbar.Toggle/>
       <Navbar.Collapse>
       <Nav className='nav-items--'>
-        <Nav.Link smooth href='#Home'><h4 className='nav-link'>HOME</h4></Nav.Link>
-        <Nav.Link smooth href='#About'><h4 className='nav-link'>ABOUT</h4></Nav.Link>
+        <Nav.Link href='#Home'><h4 className='nav-link'>HOME</h4></Nav.Link>
+        <Nav.Link href='#About'><h4 className='nav-link'>ABOUT</h4></Nav.Link>
         <Nav.Link href='home'><h4 className='nav-link'>PROJECT</h4></Nav.Link>
         <Nav.Link href='home'><h4 className='nav-link'>CONTACT</h4></Nav.Link>
       </Nav>
